dd65eb20078adbfaa7d16a7d5846e81637178906
James Payne <james.payne1349@gmail.com>
2020-12-29 11:50:24 -0600

    small script written to test a relay on input of user

--- valveTesting.py
+++ valveTesting.py
@@ -1,2 +1,22 @@
 # test file to run on the pi while setting up valve control
 
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD) # set up for board number, not gpio number
+GPIO.setup(37, GPIO.OUT)
+
+
+def commandLineTest():
+    while True:
+        ans_question = input('True or False')
+        if( ans_question == 'True' ):
+            GPIO.output(37, True)
+            print('relay True')
+        elif( ans_question == 'Exit' ):
+            exit()
+        else:
+            GPIO.output(37, False)
+            print('relay False')
+        
