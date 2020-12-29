52ddd0ce190f51c71f6f279517985bcf100befb0
James Payne <james.payne1349@gmail.com>
2020-12-29 17:46:20 -0600

    worked, needed a couple changes

--- valveTesting.py
+++ valveTesting.py
@@ -6,45 +6,47 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD) # set up for board number, not gpio number
 
-board_pins = [11,13,15,16]
+board_pins = [11,15,13,16]
 GPIO.setup(board_pins, GPIO.OUT, initial=GPIO.HIGH)
 
 on = False
 off = True
 
+def printStatus():
+    print('ONE:', GPIO.input(board_pins[0]), '  TWO:', GPIO.input(board_pins[1]),
+                '  THREE:', GPIO.input(board_pins[2]), '  FOUR:', GPIO.input(board_pins[3]))
+
 def commandLineTest():
     while True:
         ans_question = input('Relay # :')
-        print('1:', GPIO.input(board_pins[0]), '2:', GPIO.input(board_pins[1]),
-                '3:', GPIO.input(board_pins[2]), '4:', GPIO.input(board_pins[3]))
         if( ans_question == '1' ):
             if( GPIO.input(board_pins[0]) == 1 ):
                 GPIO.output(board_pins[0], on)
-                print('relay one ON')
+                printStatus()
             else:
                 GPIO.output(board_pins[0], off)
-                print('relay one OFF')        
+                printStatus()      
         elif( ans_question == '2' ):
             if( GPIO.input(board_pins[1]) == 1 ):
                 GPIO.output(board_pins[1], on)
-                print('relay two ON')
+                printStatus() 
             else:
                 GPIO.output(board_pins[1], off)
-                print('relay two OFF')  
+                printStatus()   
         elif( ans_question == '3' ):
             if( GPIO.input(board_pins[2]) == 1 ):
                 GPIO.output(board_pins[2], on)
-                print('relay three ON')
+                printStatus() 
             else:
                 GPIO.output(board_pins[2], off)
-                print('relay three OFF') 
+                printStatus() 
         elif( ans_question == '4' ):
             if( GPIO.input(board_pins[3]) == 1 ):
                 GPIO.output(board_pins[3], on)
-                print('relay four ON')
+                printStatus() 
             else:
                 GPIO.output(board_pins[3], off)
-                print('relay four OFF')
+                printStatus() 
         else:
             print('input not valid')
 
