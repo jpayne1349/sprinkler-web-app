052b9032c63fce9b92407edfcfb011d29e36c936
James Payne <james.payne1349@gmail.com>
2021-03-24 18:38:06 -0500

    changing request

--- app/main_blueprint/main.py
+++ app/main_blueprint/main.py
@@ -9,7 +9,7 @@ main_blueprint = Blueprint('main_blueprint', __name__)
 @main_blueprint.route('/', methods=['GET','POST'])
 def homepage():
 
-    post_data = request.get_json()
+    post_data = request.data
 
     print(post_data)
 
