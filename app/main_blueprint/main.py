8903f203fd081615ff5a78d19a6dab0f57a52851
James Payne <james.payne1349@gmail.com>
2021-03-24 17:25:39 -0500

    starting work on an api instead

--- app/main_blueprint/main.py
+++ app/main_blueprint/main.py
@@ -1,11 +1,16 @@
 
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, request
 
 from flask import current_app as app
 
 main_blueprint = Blueprint('main_blueprint', __name__) 
 
 
-@main_blueprint.route('/')
+@main_blueprint.route('/', methods=['GET','POST'])
 def homepage():
+
+    post_data = request.get_json()
+
+    print(post_data)
+
     return render_template('homepage.html')
