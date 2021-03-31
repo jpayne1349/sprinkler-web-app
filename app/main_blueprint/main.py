7a58849b31896466429592f137d01a3c386c7968
James Payne <james.payne1349@gmail.com>
2021-03-31 15:28:59 -0500

    timeout to 20 minutes

--- app/main_blueprint/main.py
+++ app/main_blueprint/main.py
@@ -18,7 +18,7 @@ def homepage():
 
     if state == 'on':
         print('turning sprinklers on')
-        sprinkler_job = current_app.task_queue.enqueue(runSprinklers.run, 5, job_timeout=600, job_id='sprinkler_job')
+        sprinkler_job = current_app.task_queue.enqueue(runSprinklers.run, 5, job_timeout=1200, job_id='sprinkler_job')
         print('job id = ', sprinkler_job.get_id())
         print('job status = ', sprinkler_job.get_status())
         return '1'
