1d912a5f225817bed06b0f6d8cb0efd66f0ad145
James Payne <james.payne1349@gmail.com>
2021-03-30 20:37:45 -0500

    0.8

--- app/main_blueprint/main.py
+++ app/main_blueprint/main.py
@@ -10,7 +10,6 @@ import asyncio
 main_blueprint = Blueprint('main_blueprint', __name__) 
 
 task_loop = asyncio.new_event_loop()
-task_loop.run_forever()
 
 @main_blueprint.route('/', methods=['GET','POST'])
 def homepage():
@@ -29,7 +28,8 @@ def homepage():
         if any_running is not None:
             return 1
         
-        task_loop.create_task(async_running.run_sprinklers(5))
+        task_loop.run_until_complete(async_running.run_sprinklers(5))
+        #task_loop.create_task(async_running.run_sprinklers(5))
         
         # start up the asynchronous function
         # return 1 to on 
