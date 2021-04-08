b660ea280d22de9938d10d31f7c7bb2287a8ef0e
James Payne <james.payne1349@gmail.com>
2021-04-08 11:41:45 -0500

    changing to fake job for a test

--- app/main_blueprint/main.py
+++ app/main_blueprint/main.py
@@ -21,7 +21,7 @@ def homepage():
 
     if state == 'on':
         print('turning sprinklers on')
-        sprinkler_job = current_app.task_queue.enqueue(runSprinklers.run, 5, job_timeout=1200, job_id='sprinkler_job')
+        sprinkler_job = current_app.task_queue.enqueue(runSprinklers.fake_run, 5, job_timeout=1200, job_id='sprinkler_job')
         print('job id = ', sprinkler_job.get_id())
         print('job status = ', sprinkler_job.get_status())
         return '1'
